a9dcafa6830b62f34c1e007682e4876cb45d264c
Hasaranga Dias <142646474+hasarangad@users.noreply.github.com>
2026-05-17 23:12:43 +0530

    deploy

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Set this to your GitHub repository name.
+  // For example, if your repository URL is https://github.com/username/anniversary
+  // the base should be '/anniversary/'
+  base: '/Anniversary/',
 })
